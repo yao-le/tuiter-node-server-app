8208772d821114c9ec62e38b9ef0eaf630343fee
yao-le <le.yao@northeastern.edu>
2023-03-25 13:20:10 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from 'cors'
+
+import HelloController from "./controllers/hello-controller.js";
+import UsersController from "./controllers/users/users-controller.js";
+import TuitsController from "./controllers/tuits/tuits-controller.js";
+
+const app = express();
+
+app.use(cors());
+// middleware: parse JSON from HTTP request body
+app.use(express.json());
+
+TuitsController(app);
+HelloController(app);
+UsersController(app);
+
+app.listen(process.env.PORT || 4000);
